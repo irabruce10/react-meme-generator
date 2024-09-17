035dc4715b96a913a2ff162a2bdd1dc492e6ba22
Bruce <irabruce10@gmail.com>
2024-09-17 21:34:23 +0200

    Fixing some bug

--- src/App.js
+++ src/App.js
@@ -51,11 +51,9 @@ export default function App() {
 
       <button onClick={generateMeme}>Preview</button>
 
-      {/* <button type="submit">Fetch Memes</button> */}
-
       <div>
         {memeList && (
-          <img src={memeList} data-test-id="meme-image" alt="Generated meme" />
+          <img data-test-id="meme-image" src={memeList} alt="Generated meme" />
         )}
 
         <button onClick={downloadMeme}>Download</button>
@@ -63,74 +61,3 @@ export default function App() {
     </div>
   );
 }
-
-// import React, { useState } from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './App.css';
-
-// export default function App() {
-//   const [topText, setTopText] = useState('');
-//   const [bottomText, setBottomText] = useState('');
-//   const [memeTemplate, setMemeTemplate] = useState('doge');
-//   const [memeUrl, setMemeUrl] = useState('');
-
-//   const handleTopTextChange = (e) => {
-//     setTopText(e.target.value);
-//   };
-
-//   const handleBottomTextChange = (e) => {
-//     setBottomText(e.target.value);
-//   };
-
-//   const handleMemeTemplateChange = (e) => {
-//     setMemeTemplate(e.target.value);
-//   };
-
-//   const generateMeme = () => {
-//     const url = `https://memegen.link/${memeTemplate}/${topText}/${bottomText}.png`;
-//     setMemeUrl(url);
-//   };
-
-//   const downloadMeme = () => {
-//     const link = document.createElement('a');
-//     link.href = memeUrl;
-//     link.download = `${memeTemplate}.png`;
-//     link.click();
-//   };
-
-//   return (
-//     <div className="App">
-//       <label htmlFor="top-text">Top text</label>
-//       <input
-//         id="top-text"
-//         type="text"
-//         value={topText}
-//         onChange={handleTopTextChange}
-//       />
-
-//       <label htmlFor="bottom-text">Bottom text</label>
-//       <input
-//         id="bottom-text"
-//         type="text"
-//         value={bottomText}
-//         onChange={handleBottomTextChange}
-//       />
-
-//       <label htmlFor="meme-template">Meme template</label>
-//       <input
-//         id="meme-template"
-//         type="text"
-//         value={memeTemplate}
-//         onChange={handleMemeTemplateChange}
-//       />
-
-//       <button onClick={generateMeme}>Preview</button>
-
-//       {memeUrl && (
-//         <img data-test-id="meme-image" src={memeUrl} alt="Generated meme" />
-//       )}
-
-//       <button onClick={downloadMeme}>Download</button>
-//     </div>
-//   );
-// }
