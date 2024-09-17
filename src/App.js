6dbc2844637276b107a8f9a2bfd4501e4dd9b064
Bruce <irabruce10@gmail.com>
2024-09-17 15:57:25 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,43 @@
+import { useState, useEffect } from 'react';
+
 export default function App() {
-  return <div>Hello World</div>;
+  const [memeList, setmemeList] = useState([]);
+  const url = 'https://api.memegen.link/images/';
+
+  useEffect(() => {
+    async function getMemesApi() {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setmemeList(data);
+      } catch (error) {
+        console.error('Error fetching memes:', error);
+      }
+    }
+    getMemesApi();
+  }, []);
+
+  function memesHandler() {
+    // const randomNumber = Math.floor(Math.random() * memeList.length);
+    // let randomMemeURL = memeList[randomNumber].attributes.data.attributes.url;
+    // setrandomMeme(randomMemeURL);
+    const rand = memeList[Math.floor(Math.random() * memeList.length)].url;
+
+    // Create an image element and append it to the body
+    const img = document.createElement('img');
+    img.src = rand;
+    document.body.appendChild(img);
+    console.log(rand);
+  }
+
+  return (
+    <div>
+      <h1>Memes</h1>
+      <div></div>
+
+      <img src={memeList} alt="Meme" />
+
+      <button onClick={memesHandler}>Fetch Memes</button>
+    </div>
+  );
 }
